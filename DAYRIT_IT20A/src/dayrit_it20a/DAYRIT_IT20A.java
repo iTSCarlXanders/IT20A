5d6e6f8235e0729227bb559b6f2f263b0064b7b8
iTSCarlXanders <xanderdayrit6@gmail.com>
2025-09-04 15:16:46 +0800

    Initial commit: Hello World

---
+++ DAYRIT_IT20A/src/dayrit_it20a/DAYRIT_IT20A.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package dayrit_it20a;
+
+/**
+ *
+ * @author PC-16
+ */
+public class DAYRIT_IT20A {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!");
+    }
+    
+}
